76dce0210b80c6a0a8fe4922ce6a5c6ac547f068
Evan Gyori <evangyori@gmail.com>
2024-09-04 20:10:09 -0700

    Added cross product method to Vector3D
    The cross product will be used to compute the normal vector of a surface
    using the partial derivatives of the position vector.

--- Vector3D.cs
+++ Vector3D.cs
@@ -18,11 +18,22 @@ public struct Vector3D
 		return System.Math.Sqrt(x * x + y * y + z * z);
 	}
 
+	// Changes the length of the vector to one while maintaining its direction
 	public void Normalize()
 	{
 		double length = GetLength();
 		x /= length;
 		y /= length;
 		z /= length;
+	}
+	
+	// Returns a cross b. a X b
+	public static Vector3D CrossProduct(Vector3D a, Vector3D b)
+	{
+		return new Vector3D(
+			a.GetY() * b.GetZ() - a.GetZ() * b.GetY(),
+			a.GetZ() * b.GetX() - a.GetX() * b.GetZ(),
+			a.GetX() * b.GetY() - a.GetY() * b.GetX()
+			);
 	}
 }
