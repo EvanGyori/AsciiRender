c05de510c8bf083caa85c0e1539aa26b3fe1fbe8
Evan Gyori <evangyori@gmail.com>
2024-09-23 18:42:35 -0700

    Added rotations about z axis

--- Surfaces/Decorators/Rotation.cs
+++ Surfaces/Decorators/Rotation.cs
@@ -3,52 +3,61 @@ using Math = System.Math;
 
 public class Rotation : Decorator
 {
-	double yzAngle, xzAngle;
+	double yzAngle, xzAngle, xyAngle;
 	
-	public Rotation(double yzAngle, double xzAngle, Surface next) : base(next)
+	public Rotation(double yzAngle, double xzAngle, double xyAngle, Surface next) : base(next)
 	{
-		this.yzAngle = yzAngle;
-		this.xzAngle = xzAngle;
+		SetAngles(yzAngle, xzAngle, xyAngle);
 	}
 	
 	public override Vector3D GetPosition(double u, double v, double time)
 	{
-		return RotateAboutYAxis(RotateAboutXAxis(base.GetPosition(u, v, time)));
+		return RotateAboutZAxis(RotateAboutYAxis(RotateAboutXAxis(base.GetPosition(u, v, time))));
 	}
 	
 	public override Vector3D GetDerivativeWithU(double u, double v, double time)
 	{
 		// The derivative of the rotation is obtained by just passing the derivative since the rotations
 		// are linear in addition of the functions
-		return RotateAboutYAxis(RotateAboutXAxis(base.GetDerivativeWithU(u, v, time)));
+		return RotateAboutZAxis(RotateAboutYAxis(RotateAboutXAxis(base.GetDerivativeWithU(u, v, time))));
 	}
 
 	public override Vector3D GetDerivativeWithV(double u, double v, double time)
 	{
-		return RotateAboutYAxis(RotateAboutXAxis(base.GetDerivativeWithV(u, v, time)));
+		return RotateAboutZAxis(RotateAboutYAxis(RotateAboutXAxis(base.GetDerivativeWithV(u, v, time))));
 	}
 	
-	public void SetAngles(double yzAngle, double xzAngle)
+	public void SetAngles(double yzAngle, double xzAngle, double xyAngle)
 	{
 		this.yzAngle = yzAngle;
 		this.xzAngle = xzAngle;
+		this.xyAngle = xyAngle;
 	}
 	
 	Vector3D RotateAboutXAxis(Vector3D position)
 	{
 		return new Vector3D(
 			position.GetX(),
-			position.GetY() * Math.Cos(yzAngle) + position.GetZ() * Math.Sin(yzAngle),
-			-1 * position.GetY() * Math.Sin(yzAngle) + position.GetZ() * Math.Cos(yzAngle)
+			position.GetY() * Math.Cos(yzAngle) - position.GetZ() * Math.Sin(yzAngle),
+			position.GetY() * Math.Sin(yzAngle) + position.GetZ() * Math.Cos(yzAngle)
 		);
 	}
 	
 	Vector3D RotateAboutYAxis(Vector3D position)
 	{
 		return new Vector3D(
-			position.GetX() * Math.Cos(xzAngle) + position.GetZ() * Math.Sin(xzAngle),
+			position.GetX() * Math.Cos(xzAngle) - position.GetZ() * Math.Sin(xzAngle),
 			position.GetY(),
-			-1 * position.GetX() * Math.Sin(xzAngle) + position.GetZ() * Math.Cos(xzAngle)
+			position.GetX() * Math.Sin(xzAngle) + position.GetZ() * Math.Cos(xzAngle)
+		);
+	}
+	
+	Vector3D RotateAboutZAxis(Vector3D position)
+	{
+		return new Vector3D(
+			position.GetX() * Math.Cos(xyAngle) - position.GetY() * Math.Sin(xyAngle),
+			position.GetX() * Math.Sin(xyAngle) + position.GetY() * Math.Cos(xyAngle),
+			position.GetZ()
 		);
 	}
 }
--- Surfaces/Decorators/Spinning.cs
+++ Surfaces/Decorators/Spinning.cs
@@ -3,17 +3,18 @@ using Math = System.Math;
 
 public class Spinning : Rotation
 {
-	double yzRate, xzRate;
+	double yzRate, xzRate, xyRate;
 	
-	public Spinning(double yzRate, double xzRate, Surface next) : base(0.0, 0.0, next)
+	public Spinning(double yzRate, double xzRate, double xyRate, Surface next) : base(0, 0, 0, next)
 	{
 		this.yzRate = yzRate;
 		this.xzRate = xzRate;
+		this.xyRate = xyRate;
 	}
 	
 	public override Vector3D GetPosition(double u, double v, double time)
 	{
-		base.SetAngles(yzRate * time, xzRate * time);
+		base.SetAngles(yzRate * time, xzRate * time, xyRate * time);
 		return base.GetPosition(u, v, time);
 	}
 }
